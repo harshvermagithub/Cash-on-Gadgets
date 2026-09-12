5a9da71bd55c6e575760e05ab189dc589fac4687
Harsh Verma <50040437+harshvermagithub@users.noreply.github.com>
2026-09-12 19:37:08 +0530

    feat(analytics): add Google tag (gtag.js AW-18446569040) to root layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,11 @@ export const metadata: Metadata = {
 };
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import InitialLoader from "@/components/ui/InitialLoader";
 import { ThemeProvider } from "@/components/theme-provider";
 import HeaderVisibility from "@/components/HeaderVisibility";
 
-import Footer from "@/components/Footer";
-import InitialLoader from "@/components/ui/InitialLoader";
 
 export default function RootLayout({
   children,
@@ -31,6 +31,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18446569040"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18446569040');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
         suppressHydrationWarning
